2d7ed7601057b704e15bf6c0676664fef4d89436
falkTX <falktx@falktx.com>
2022-11-21 11:41:11 +0000

    Set license for convolver
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/ConvolutionReverb/OneKnobPlugin.cpp
+++ plugins/ConvolutionReverb/OneKnobPlugin.cpp
@@ -2,16 +2,16 @@
  * DISTRHO OneKnob Convolution Reverb
  * Copyright (C) 2022 Filipe Coelho <falktx@falktx.com>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation.
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with
+ * or without fee is hereby granted, provided that the above copyright notice and this
+ * permission notice appear in all copies.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * For a full copy of the license see the LICENSE file.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD
+ * TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN
+ * NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 // IDE helper (not needed for building)
@@ -129,8 +129,7 @@ protected:
 
     const char* getLicense() const noexcept override
     {
-        // TODO
-        return "";
+        return "ISC";
     }
 
     int64_t getUniqueId() const noexcept override
